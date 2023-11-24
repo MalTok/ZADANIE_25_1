b0eb4f59cd1f378f0eb915a15407ead9fc2c3b3d
MalTok <mig01@wp.pl>
2023-11-24 12:55:57 +0100

    Utworzenie zadania

---
+++ src/main/java/pl/mt/taskmanager/Zadanie251Application.java
@@ -0,0 +1,13 @@
+package pl.mt.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie251Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie251Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mt/taskmanager/Zadanie251ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mt.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie251ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
